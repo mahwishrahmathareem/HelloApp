0e9df9974b4d15bdd11d0f09e87ef0915a79beb4
Mahwish <mahwishrahmathareem@gmail.com>
2026-02-22 22:46:30 +0530

    Restore UC1 alongside UC2

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -7,13 +7,8 @@
  * @version 1.0
  */
 public class OOPSBannerApp {
+
     public static void main(String[] args) {
-        System.out.println("   ***     ***   ******   ***** ");
-        System.out.println(" **   ** **   ** **   ** **   **");
-        System.out.println(" **   ** **   ** **   ** **     ");
-        System.out.println(" **   ** **   ** ******    ***  ");
-        System.out.println(" **   ** **   ** **           **");
-        System.out.println(" **   ** **   ** **      **   **");
-        System.out.println("   ***     ***   **       ***** ");
-}
+        System.out.println("OOPS");
     }
+}
