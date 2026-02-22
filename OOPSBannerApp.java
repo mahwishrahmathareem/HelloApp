952e48d60267a10df6b08da6d556b1b4ce247a04
Mahwish <mahwishrahmathareem@gmail.com>
2026-02-22 22:24:23 +0530

    UC2: Print OOPS Banner

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -9,6 +9,14 @@
 public class OOPSBannerApp {
 
     public static void main(String[] args) {
-        System.out.println("OOPS");
-    }
+
+    
+        System.out.println("   ***     ***   ******   ***** ");
+        System.out.println(" **   ** **   ** **   ** **   **");
+        System.out.println(" **   ** **   ** **   ** **     ");
+        System.out.println(" **   ** **   ** ******    ***  ");
+        System.out.println(" **   ** **   ** **           **");
+        System.out.println(" **   ** **   ** **      **   **");
+        System.out.println("   ***     ***   **       ***** ");
 }
+    }
